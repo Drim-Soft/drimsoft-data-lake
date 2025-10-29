641fcb7578f5ae266b8e5ed9d1a0bc389888176d
JuanSe <jsebastiannovoa@gmail.com>
2025-10-29 00:23:30 -0500

    Fix: Implemented .env

--- etl/main.py
+++ etl/main.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# URIs de Supabase (externos)
-DB_URI_DRIMSOFT = "postgresql://postgres.zqbwjlrlnxjlrusmmciw:Drimsoft2025.@aws-1-us-east-2.pooler.supabase.com:6543/postgres?sslmode=require"
-DB_URI_PLANIFIKA = "postgresql://postgres.znzlfnztvnnzfbbsdjsl:Planifika2025.@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
-DB_URI_PROYECTOS = "postgresql://postgres.rniqzbnygegbkecikbxj:Planifika2025.@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
-DB_URI_SUSCRIPCIONES = "postgresql://postgres.iwnxlkmjvxmcuscangyi:Drimsoft2025.@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require"
+# Cargar variables del archivo .env
+load_dotenv()
 
-# Engines
+# Leer las variables
+DB_URI_DRIMSOFT = os.getenv("DB_URI_DRIMSOFT")
+DB_URI_PLANIFIKA = os.getenv("DB_URI_PLANIFIKA")
+DB_URI_PROYECTOS = os.getenv("DB_URI_PROYECTOS")
+DB_URI_SUSCRIPCIONES = os.getenv("DB_URI_SUSCRIPCIONES")
+
+# Crear engines
 engine_drimsoft = create_engine(DB_URI_DRIMSOFT, pool_pre_ping=True, pool_recycle=3600, echo=False)
 engine_planifika = create_engine(DB_URI_PLANIFIKA, pool_pre_ping=True, pool_recycle=3600, echo=False)
 engine_proyectos = create_engine(DB_URI_PROYECTOS, pool_pre_ping=True, pool_recycle=3600, echo=False)
